da96ec7b07be4ebbd547830d8539a84529362c8f
Zhairov Abulkhair <zz.abulhair.zz@gmail.com>
2025-11-24 23:40:05 +0500

    settings.py updated

--- crud_app/settings.py
+++ crud_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0mi-q&%*+oxz#q2)j)9a6p3f_h=(cdj&7%ge=t*&ud_71xz%_v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myapp.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
